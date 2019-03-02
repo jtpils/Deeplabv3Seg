055fd05ede8f2b034c3604ff61f263ee413d321e
SeriouslyHAO <46627631+SeriouslyHAO@users.noreply.github.com>
2019-03-02 10:43:33 +0800

    Update eval.py

--- eval.py
+++ eval.py
@@ -1,9 +1,11 @@
+#==============================================================================
 # BSD 3-Clause License
 #
 # Copyright (c) 2019, FPAI
 # Copyright (c) 2019, SeriouslyHAO
 # Copyright (c) 2019, xcj2019
 # Copyright (c) 2019, Leonfirst
+# Copyright (c) 2017, Bichen Wu
 #
 #
 # All rights reserved.
@@ -32,9 +34,7 @@
 # CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-"""Evaluation"""
-
+#=============================================================================
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
